3314924819458d5e08f7561eacded5dd2e51d2cc
VaibhavBansal26 <vaibhav.bansal945@gmail.com>
2025-02-27 17:32:34 -0500

    inference pipeline isssues sorted

--- src/inference.py
+++ src/inference.py
@@ -135,7 +135,8 @@ def fetch_hourly_rides(hours):
 def fetch_days_data(days):
     current_date = pd.to_datetime(datetime.now(timezone.utc))
     fetch_data_from = current_date - timedelta(days=(365 + days))
-    fetch_data_to = current_date - timedelta(days=365)
+    fetch_data_to = current_date 
+    # - timedelta(days=365)
     print(fetch_data_from, fetch_data_to)
     fs = get_feature_store()
     fg = fs.get_feature_group(name=config.FEATURE_GROUP_NAME, version=1)
